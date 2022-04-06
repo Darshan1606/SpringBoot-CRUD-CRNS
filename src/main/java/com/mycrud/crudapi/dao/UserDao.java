fbce692b6676b0d935a8eba0d3d6a7616604aa5d
Darshan Tarsariya <darshantarsariya17@gmail.com>
2022-04-06 23:50:09 +0530

    update dao

--- src/main/java/com/mycrud/crudapi/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.mycrud.crudapi.dao;
-
-import com.mycrud.crudapi.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDao extends CrudRepository<User, Integer> {
-}
